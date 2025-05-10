d51f28047ba42409cac2fa512296a6a9e1a43fda
Aditya Kokashe <adi310kokashe@gmail.com>
2025-05-10 13:05:46 +0000

    updating state of object

--- react1/src/mycomponent.jsx
+++ react1/src/mycomponent.jsx
@@ -2,28 +2,28 @@ import React, {useState} from 'react';
 
 function MyComponent (){
 
-    const [car , setCar] = useState({year: 2024, 
+    const [car, setCar] = useState({year: 2024, 
                                     model: "Mustang", 
                                     make: "Ford"}); 
 
     function handleCarYear (event) {
-        setCar({...car, year: 2025});
+        setCar(c => ({...c, year : event.target.value}));
     }  
     
-    function handleCarMake (event) {
-        setCar();
+    function handleCarModel (event) {
+        setCar(c => ({...c, model : event.target.value}));
     }     
 
-    function handleCarModel (event) {
-        setCar();
+    function handleCarMake (event) {
+        setCar(c => ({...c, make : event.target.value}));
     }     
 
     return (<div>
                 <p>my favourite car is {car.make} - {car.model} of year: {car.year}</p>
 
-                <input type="number" value={car.year} onchange={handleCarYear} /><br/>
-                <input type="text" value={car.model} onchange={handleCarModel} /><br/>
-                <input type="text" value={car.make} onchange={handleCarMake} /><br/>
+                <input type="number" value={car.year} onChange={handleCarYear} /><br/>
+                <input type="text" value={car.model} onChange={handleCarModel} /><br/>
+                <input type="text" value={car.make} onChange={handleCarMake} /><br/>
             </div>);                                
 
 }
